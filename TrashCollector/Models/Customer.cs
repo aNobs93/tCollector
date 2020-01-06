d4c248a0ca3334f07585521c0eb5c249b38a34b8
Adam Nobs <nobsadam@gmail.com>
2020-01-06 15:27:38 -0600

    Migrated my database and have my customer and employee models set with properties

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -8,28 +8,30 @@ using System.Web;
 namespace TrashCollector.Models
 {
     public class Customer
-    {
+    {        
+        public Customer()
+        {
+
+        }
         [Key]
-        int ID { get; set; }        
+        public int ID { get; set; }        
         
         [ForeignKey("ApplicationUser")]
         public string ApplicationId { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
 
-        int PickUpDay { get; set; }
-        string FirstName { get; set; }
-        string LastName { get; set; }
-        string StreetAddress { get; set; }
-        string City { get; set; }
-        string State { get; set; }
-        int ZipCode { get; set; }
-        double Balance { get; set; }
-        string ExtraPickUpDate { get; set; }
-        string SuspendedStart { get; set; }
-        string SuspendedEnd { get; set; }
-        bool PickUpConfirmation { get; set; }
-
-
+        public int PickUpDay { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public double Balance { get; set; }
+        public string ExtraPickUpDate { get; set; }
+        public string SuspendedStart { get; set; }
+        public string SuspendedEnd { get; set; }
+        public bool PickUpConfirmation { get; set; }
 
     }
 }
--- TrashCollector/Models/Employee.cs
+++ TrashCollector/Models/Employee.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,19 @@ namespace TrashCollector.Models
 {
     public class Employee
     {
+        public Employee()
+        {
+
+        }
+        [Key]
+        public int ID { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ZipCode { get; set; }
+        
+
     }
 }
